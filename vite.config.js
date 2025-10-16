c3866d12d998bc86be093904d863e331fd3ac1e2
00RAM00 <ramgamer52@gmail>
2025-10-16 10:28:05 -0500

    prueba

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/Mini-Project1-React/', // 👈 importante: debe tener slash inicial y final
-})
